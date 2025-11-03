c42587dfefc9c8126e85928f15ecaa5237a34de7
PJChristian <dgchristian10@gmail.com>
2025-11-03 13:40:02 +0800

    API Versioning

--- backend-laravel/routes/api_v1.php
+++ backend-laravel/routes/api_v1.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\V1\CarController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('cars', CarController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
